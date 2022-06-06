235ddee87f2a8f48588a6a8cfa9338dcec53f201
Ivan Chuchulski <zake6@abv.bg>
2022-06-06 16:21:14 +0300

    begin index page

--- client/src/main/java/password/vault/client/gui/controllers/IndexController.java
+++ client/src/main/java/password/vault/client/gui/controllers/IndexController.java
@@ -4,12 +4,15 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Orientation;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
 import password.vault.api.CredentialIdentifierDTO;
 import password.vault.api.Response;
+import password.vault.api.ServerResponses;
 import password.vault.api.ServerTextCommandsFactory;
 import password.vault.client.communication.Client;
 import password.vault.client.gui.CommonUIElements;
@@ -19,6 +22,7 @@ import password.vault.client.gui.StageManager;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,6 +34,9 @@ public class IndexController {
 
     @FXML
     private Button btnLogout;
+
+    @FXML
+    private FlowPane flowPane;
 
     @FXML
     private Label lblWelcome;
@@ -46,19 +53,37 @@ public class IndexController {
     void initialize() {
         lblWelcome.setText(lblWelcome.getText() + username + "!");
 
-        try {
-            client.sendRequest(ServerTextCommandsFactory.getAllCredentialsJSON());
-            Response response = client.receiveResponse();
-            System.out.println(response.message());
+        Response response = fetchCredentials();
 
-            Type listType = new TypeToken<List<CredentialIdentifierDTO>>() {}.getType();
-            List<CredentialIdentifierDTO> credentials = gson.fromJson(response.message(), listType);
-
-            System.out.println(credentials);
-        } catch (IOException e) {
-            e.printStackTrace();
-            CommonUIElements.getErrorAlert("error fetching data from server");
+        if (response == null) {
+            return;
         }
+
+        if (!response.serverResponse().equals(ServerResponses.CREDENTIAL_RETRIEVAL_SUCCESS)) {
+            CommonUIElements.getErrorAlert("error fetching data from server" + response.message());
+            return;
+        }
+
+        Type listType = new TypeToken<List<CredentialIdentifierDTO>>() {
+        }.getType();
+        List<CredentialIdentifierDTO> credentials = gson.fromJson(response.message(), listType);
+
+        System.out.println(credentials);
+
+        List<Credential> guiCredentials = new LinkedList<>();
+        for (CredentialIdentifierDTO credential : credentials) {
+            Credential guiCredential = new Credential();
+
+            guiCredential.getLblDomain().setText(credential.getWebsite());
+            guiCredential.getLblUsername().setText(credential.getUsernameForWebsite());
+
+            guiCredentials.add(guiCredential);
+        }
+
+        flowPane.setOrientation(Orientation.VERTICAL);
+        flowPane.setVgap(10);
+        flowPane.setHgap(10);
+        flowPane.getChildren().addAll(guiCredentials);
     }
 
     @FXML
@@ -89,5 +114,19 @@ public class IndexController {
         context.setLoggedInUsername("");
         StageManager stageManager = context.getStageManager();
         stageManager.switchScene(FXMLScenes.LOGIN);
+    }
+
+    private Response fetchCredentials() {
+        try {
+            client.sendRequest(ServerTextCommandsFactory.getAllCredentialsJSON());
+            Response response = client.receiveResponse();
+            System.out.println(response.message());
+
+            return response;
+        } catch (IOException e) {
+            e.printStackTrace();
+            CommonUIElements.getErrorAlert("error fetching data from server");
+            return null;
+        }
     }
 }
